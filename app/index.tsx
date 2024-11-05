00879a790aec4789ad925b7d503138fb17e04bce
CeciliaHalbritter <cecilia.halbritter@deutschebahn.com>
2024-11-05 14:49:19 +0100

    Ui Kitten integration, Color Theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import {View} from "react-native";
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, Button, Text} from '@ui-kitten/components';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Button>BUTTON</Button>
+      </View>
+    </ApplicationProvider>
   );
 }

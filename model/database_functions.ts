e622ba7d4ce4bee523f14911da16893e23fa8cd4
Simon Johannes Engelhardt <114220950+SimonJohannesEngelhardt@users.noreply.github.com>
2024-11-06 15:13:50 +0100

    GPLR-59 Offline Datenspeicherung (#13)
    Watermelon DB implementierung
    
    ---------
    
    Co-authored-by: MarcoCoreDuo <90222533+MarcoCoreDuo@users.noreply.github.com>
    Co-authored-by: Simon Johannes Engelhardt <simon-johannes.engelhardt@deutschebahn.com>

---
+++ model/database_functions.ts
@@ -0,0 +1,63 @@
+import { database } from "./createDatabase";
+import { Journey, Trip, Location } from "./model";
+
+const createJourney = async (title: string): Promise<Journey> => {
+  return database.write(async () => {
+    return database.get<Journey>("journeys").create((journey) => {
+      journey.title = title;
+    });
+  });
+};
+
+const createTrip = async (journeyId: string, title: string): Promise<Trip> => {
+  return database.write(async () => {
+    const journey = await database.get<Journey>("journeys").find(journeyId);
+    return database.get<Trip>("trips").create((trip) => {
+      trip.journey.set(journey);
+      trip.title = title;
+    });
+  });
+};
+
+const createLocation = async (
+  tripId: string,
+  latitude: number,
+  longitude: number,
+): Promise<Location> => {
+  return database.write(async () => {
+    const trip = await database.get<Trip>("trips").find(tripId);
+    return database.get<Location>("locations").create((location) => {
+      location.trip.set(trip);
+      location.latitude = latitude;
+      location.longitude = longitude;
+      location.recordedAt = Date.now();
+    });
+  });
+};
+
+const getAllJourneysQuery = database.get<Journey>("journeys").query();
+const getAllJourneys = () => getAllJourneysQuery.fetch();
+
+const getAllTripsByJourneyId = (journeyId: string) => {
+  return database.get<Trip>("trips").find(journeyId);
+};
+const getAllLocationsByTripId = (tripId: string) => {
+  return database.get<Location>("locations").find(tripId);
+};
+
+const deleteAllJourneys = () => {
+  void database.write(async () => {
+    const journeys = await getAllJourneys();
+    journeys.forEach((journey) => journey.destroyPermanently());
+  });
+};
+
+export {
+  createJourney,
+  createTrip,
+  createLocation,
+  getAllJourneysQuery,
+  getAllTripsByJourneyId,
+  getAllLocationsByTripId,
+  deleteAllJourneys,
+};
---
+++ model/schema.ts
@@ -0,0 +1,38 @@
+import { appSchema, tableSchema } from "@nozbe/watermelondb";
+
+export const schema = appSchema({
+  version: 1,
+  tables: [
+    tableSchema({
+      name: "journeys",
+      columns: [
+        { name: "title", type: "string" },
+        { name: "started_at", type: "number", isOptional: true },
+        { name: "finished_at", type: "number", isOptional: true },
+        { name: "average_speed", type: "number", isOptional: true },
+        { name: "average_distance_per_trip", type: "number", isOptional: true },
+        { name: "distance", type: "number", isOptional: true },
+      ],
+    }),
+    tableSchema({
+      name: "trips",
+      columns: [
+        { name: "title", type: "string" },
+        { name: "started_at", type: "number", isOptional: true },
+        { name: "finished_at", type: "number", isOptional: true },
+        { name: "distance", type: "number", isOptional: true },
+        { name: "average_speed", type: "number", isOptional: true },
+        { name: "journey_id", type: "string", isIndexed: true },
+      ],
+    }),
+    tableSchema({
+      name: "locations",
+      columns: [
+        { name: "latitude", type: "number" },
+        { name: "longitude", type: "number" },
+        { name: "recorded_at", type: "number", isOptional: true },
+        { name: "trip_id", type: "string", isIndexed: true },
+      ],
+    }),
+  ],
+});

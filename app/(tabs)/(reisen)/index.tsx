404982543fdcf6cf47c5a0a0b92c48153cfc0cfd
Simon Johannes Engelhardt <simon-johannes.engelhardt@deutschebahn.com>
2024-11-08 15:03:41 +0100

    Quick screen implementation

--- app/(tabs)/(reisen)/index.tsx
+++ app/(tabs)/(reisen)/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   getAllJourneysQuery,
   createJourney,
@@ -6,21 +6,65 @@ import {
 } from "@/model/database_functions";
 import JourneyList from "@/components/Journey/JourneyList";
 import { StyleSheet } from "react-native";
-import { Layout, Input, Button } from "@ui-kitten/components";
+import {
+  Layout,
+  Button,
+  Modal,
+  Input,
+  Text,
+  Card,
+  Datepicker,
+} from "@ui-kitten/components";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
-let journeyText = "Journey";
-
 export default function MeineReisen() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [journeyName, setJourneyName] = useState("Meine Reise");
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <Layout style={styles.container}>
-      <Input
-        placeholder="Journey Title"
-        onChangeText={(newText) => (journeyText = newText)}
-      />
-      <Button onPress={() => createJourney(journeyText)}>Create Journey</Button>
-      <Button onPress={deleteAllJourneys}>Delete All Journeys</Button>
-      <JourneyList style={{ flex: 1 }} journeys={getAllJourneysQuery} />
+      <Layout level="2">
+        <JourneyList journeys={getAllJourneysQuery} />
+        <Button
+          status="basic"
+          onPress={() => setModalVisible(true)}
+          style={{}}
+          accessoryLeft={<FontAwesomeIcon icon="plus" />}
+        >
+          Neue Reise
+        </Button>
+        <Button
+          status="basic"
+          onPress={() => deleteAllJourneys()}
+          accessoryLeft={<FontAwesomeIcon icon="trash" />}
+        >
+          Alle Reisen löschen
+        </Button>
+      </Layout>
+
+      <Modal visible={modalVisible} backdropStyle={styles.backdrop}>
+        <Card disabled={true}>
+          <Text>Bitte geben Sie ihren Reisenamen ein:</Text>
+          <Input
+            status="primary"
+            placeholder="Reisename"
+            value={journeyName}
+            onChangeText={(journeyText) => setJourneyName(journeyText)}
+          />
+          <Datepicker
+            date={startDate}
+            onSelect={(selectedDate) => setStartDate(selectedDate)}
+          />
+          <Button
+            onPress={() => {
+              createJourney(journeyName, startDate.getTime());
+              setModalVisible(false);
+            }}
+          >
+            Speichern
+          </Button>
+        </Card>
+      </Modal>
     </Layout>
   );
 }
@@ -28,7 +72,10 @@ export default function MeineReisen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
+  },
+  backdrop: {
+    backgroundColor: "rgba(0,0,0,0.5)",
   },
 });
--- components/Journey/JourneyList.tsx
+++ components/Journey/JourneyList.tsx
@@ -1,38 +1,29 @@
 import { Journey } from "@/model/model";
 import { withObservables } from "@nozbe/watermelondb/react";
-import { Layout, Text } from "@ui-kitten/components";
+import { Text } from "@ui-kitten/components";
 import { Link } from "expo-router";
-import { ScrollView, StyleSheet } from "react-native";
 
 const JourneyList = ({ journeys }: { journeys: Journey[] }) => {
   return (
-    <ScrollView style={{flex:1}}>
-      <Layout style={{ justifyContent: "center" }}>
-        {journeys.map((journey) => (
-          <Link
-            style={{
-              padding: 10,
-              borderColor: "orange",
-              borderWidth: 1,
-              borderRadius: 5,
-            }}
-            key={journey.id}
-            href={{
-              pathname: "/(tabs)/(reisen)/[journeyId]",
-              params: { journeyId: journey.id },
-            }}
-          >
-            <Layout>
-              <Text category="h3">{journey.title}</Text>
-            </Layout>
-          </Link>
-        ))}
-      </Layout>
-    </ScrollView>
+    <>
+      {journeys.map((journey) => (
+        <Link
+          style={{
+            padding: 10,
+            marginVertical: 10,
+          }}
+          key={journey.id}
+          href={{
+            pathname: "/(tabs)/(reisen)/[journeyId]",
+            params: { journeyId: journey.id },
+          }}
+        >
+          <Text category="h2">{journey.title}</Text>
+        </Link>
+      ))}
+    </>
   );
 };
-
-
 
 const enhance = withObservables(["journeys"], ({ journeys }) => ({ journeys }));
 export default enhance(JourneyList);

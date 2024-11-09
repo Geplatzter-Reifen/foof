93d7bd19d9c5803f0db71b5ce0cd50f9c7924e0f
MarcoCoreDuo <90222533+MarcoCoreDuo@users.noreply.github.com>
2024-11-10 00:07:18 +0100

    add missing dark splashscreen and fix white flashing after splashscreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [loaded] = useFonts({
+        // Pre-load custom fonts here
+    });
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
   return (
     <Stack>
       <Stack.Screen name="index" />

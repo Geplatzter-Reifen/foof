9c4fe2e97c99dc94e363010ee6b06b2b14538a20
Tobias Hermann <tobias.hermann@student.hs-rm.de>
2024-11-11 16:43:02 +0100

    Fix linting

--- services/tracking.tsx
+++ services/tracking.tsx
@@ -1,10 +1,9 @@
-import {createTrip, getActiveJourney} from "@/model/database_functions";
 import * as Location from "expo-location";
 import * as TaskManager from "expo-task-manager";
 
-const LOCATION_TASK_NAME = 'background-location-task';
-
 import { createTrip, getActiveJourney } from "@/model/database_functions";
+
+const LOCATION_TASK_NAME = "background-location-task";
 
 export async function createManualTrip(
   startingCoordinatesString,
@@ -29,36 +28,35 @@ export async function createManualTrip(
 }
 
 export async function startAutomaticTracking() {
-    const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
-    if (foregroundStatus === 'granted') {
-        console.log('Foreground status: ', foregroundStatus);
-        const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
-        if (backgroundStatus === 'granted') {
-            console.log('Background status: ', backgroundStatus);
-            if(await TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME)) {
-                console.log('Tracking already started.');
-            } else {
-                await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-                    accuracy: Location.Accuracy.Highest,
-                });
-                console.log('Tracking started.')
-            }
-        }
+  const { status: foregroundStatus } =
+    await Location.requestForegroundPermissionsAsync();
+  if (foregroundStatus === "granted") {
+    console.log("Foreground status: ", foregroundStatus);
+    const { status: backgroundStatus } =
+      await Location.requestBackgroundPermissionsAsync();
+    if (backgroundStatus === "granted") {
+      console.log("Background status: ", backgroundStatus);
+      if (await TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME)) {
+        console.log("Tracking already started.");
+      } else {
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          accuracy: Location.Accuracy.Highest,
+        });
+        console.log("Tracking started.");
+      }
     }
+  }
 }
 
 export async function stopAutomaticTracking() {
-    if(await TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME)) {
-        await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
-        console.log('Tracking stopped.');
-    } else {
-        console.log('Tracking already stopped.');
-    }
+  if (await TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME)) {
+    await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+    console.log("Tracking stopped.");
+  } else {
+    console.log("Tracking already stopped.");
+  }
 }
 
-function parseCoordinates(coordinateString: string): { latitude: number; longitude: number } | null {
-    const regex = /^\s*([-+]?\d{1,2}(?:\.\d+)?),\s*([-+]?\d{1,3}(?:\.\d+)?)\s*$/;
-    const match = coordinateString.match(regex);
 function parseCoordinates(
   coordinateString: string,
 ): { latitude: number; longitude: number } | null {
@@ -76,12 +74,12 @@ function parseCoordinates(
 }
 
 TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
-    if (error) {
-        console.log(error.message)
-        return;
-    }
-    if (data) {
-        const { locations } = data;
-        console.log('New background location: ', locations);
-    }
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    console.log("New background location: ", locations);
+  }
 });

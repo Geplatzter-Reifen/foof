77e6fda2b71725d2ae19ac98863a47ee689b8df2
Tobias Hermann <tobias.hermann@student.hs-rm.de>
2024-11-11 16:16:31 +0100

    Fix linting

--- services/tracking.tsx
+++ services/tracking.tsx
@@ -1,32 +1,39 @@
-import {createTrip, getActiveJourney} from "@/model/database_functions";
+import { createTrip, getActiveJourney } from "@/model/database_functions";
 
-export async function createManualTrip(startingCoordinatesString, endCoordinatesString) {
-    // TODO Fehlerbehandlung, falls keine Journey vorhanden
-    let activeJourney = await getActiveJourney();
-    if(activeJourney === null) {
-        throw Error("No active journey retrieved");
-    }
-    let trip = await createTrip(activeJourney.id, 'Strecke');
-    let startingCoordinates = parseCoordinates(startingCoordinatesString);
-    let endCoordinates = parseCoordinates(endCoordinatesString);
-    if(startingCoordinates === null || endCoordinates === null) {
-        throw Error("Coordinates could not be parsed");
-    }
-    await trip.addLocation(startingCoordinates?.latitude, startingCoordinates?.longitude);
-    await trip.addLocation(endCoordinates?.latitude, endCoordinates?.longitude);
+export async function createManualTrip(
+  startingCoordinatesString,
+  endCoordinatesString,
+) {
+  // TODO Fehlerbehandlung, falls keine Journey vorhanden
+  let activeJourney = await getActiveJourney();
+  if (activeJourney === null) {
+    throw Error("No active journey retrieved");
+  }
+  let trip = await createTrip(activeJourney.id, "Strecke");
+  let startingCoordinates = parseCoordinates(startingCoordinatesString);
+  let endCoordinates = parseCoordinates(endCoordinatesString);
+  if (startingCoordinates === null || endCoordinates === null) {
+    throw Error("Coordinates could not be parsed");
+  }
+  await trip.addLocation(
+    startingCoordinates?.latitude,
+    startingCoordinates?.longitude,
+  );
+  await trip.addLocation(endCoordinates?.latitude, endCoordinates?.longitude);
 }
 
-function parseCoordinates(coordinateString: string): { latitude: number; longitude: number } | null {
-    const regex = /^\s*([-+]?\d{1,2}(?:\.\d+)?),\s*([-+]?\d{1,3}(?:\.\d+)?)\s*$/;
-    const match = coordinateString.match(regex);
+function parseCoordinates(
+  coordinateString: string,
+): { latitude: number; longitude: number } | null {
+  const regex = /^\s*([-+]?\d{1,2}(?:\.\d+)?),\s*([-+]?\d{1,3}(?:\.\d+)?)\s*$/;
+  const match = coordinateString.match(regex);
 
-    if (!match) {
-        return null;
-    }
+  if (!match) {
+    return null;
+  }
 
-    const latitude = parseFloat(match[1]);
-    const longitude = parseFloat(match[2]);
+  const latitude = parseFloat(match[1]);
+  const longitude = parseFloat(match[2]);
 
-    return { latitude, longitude };
+  return { latitude, longitude };
 }
-

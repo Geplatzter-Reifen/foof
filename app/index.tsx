ab18e9462384ff1b099cd954564e914431adad4b
Tobias Hermann <tobias.hermann@student.hs-rm.de>
2024-11-11 18:03:06 +0100

    Revert index to state of develop branch

--- app/index.tsx
+++ app/index.tsx
@@ -5,29 +5,23 @@ import { ApplicationProvider, Button, Text } from "@ui-kitten/components";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { Link } from "expo-router";
 
 library.add(fas);
 
 export default function Index() {
-  return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-        <Button>BUTTON</Button>
-        <Link href={"/automatic_tracking"}>
-          Go to automatic tracking screen.
-        </Link>
-        <Link href={"/manual_tracking"}>Go to manual tracking screen.</Link>
-        <Link href={"/database_testsite"}>Go to database testsite.</Link>
-        <FontAwesomeIcon icon="bicycle" />
-      </View>
-    </ApplicationProvider>
-  );
+    return (
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Text>Edit app/index.tsx to edit this screen.</Text>
+                <Button>BUTTON</Button>
+                <FontAwesomeIcon icon="bicycle" />
+            </View>
+        </ApplicationProvider>
+    );
 }

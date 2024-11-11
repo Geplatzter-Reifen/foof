6657806aff0d97f132c0702487df3ec29976436b
annkhalus <anna.khalus@student.hs-rm.de>
2024-11-11 23:02:07 +0100

    index sache

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,6 +1,147 @@
-import { Layout } from "@ui-kitten/components";
-import React from "react";
+import React, { useState, useEffect }  from "react";
+import { View, StyleSheet, Alert  } from "react-native";
+import MapView  from 'react-native-maps';
+import MapboxGL from "@rnmapbox/maps";
+import { Layout, Text, Button } from '@ui-kitten/components';
+import * as Location from 'expo-location';
+import { startAutomaticTracking, stopAutomaticTracking } from "@/services/tracking";
+// import * as TaskManager from "expo-task-manager";
+
+
+MapboxGL.setAccessToken("pk.eyJ1Ijoia2F0emFibGFuY2thIiwiYSI6ImNtM2N4am40cTIyZnkydnNjODBldXR1Y20ifQ.q0I522XSqixPNIe6HwJdOg");
 
 export default function Index() {
-  return <Layout style={{ flex: 1 }} />;
+  // const latitude = 50.0826;
+  // const longitude = 8.24;
+  //
+  // return (
+  //     <View style={styles.container}>
+  //       <Layout style={styles.layout}>
+  //         <MapboxGL.MapView style={styles.map}>
+  //           <MapboxGL.Camera
+  //               zoomLevel={12}
+  //               centerCoordinate={[longitude, latitude]}
+  //               animationMode="flyTo"
+  //               animationDuration={2000}
+  //           />
+  //         </MapboxGL.MapView>
+  //       </Layout>
+  //     </View>
+  // );
+  const [tracking, setTracking]= useState(false)//TaskManager.isTaskRegisteredAsync("background-location-task") PROBLEM
+  const [latitude, setLatitude] = useState(50.0826); // Default to Wiesbaden
+  const [longitude, setLongitude] = useState(8.24);  // Default to Wiesbaden
+  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false)
+  useEffect(()=>{
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  },[])
+
+  const changeButton =()=>{
+    if (!tracking) {
+      startAutomaticTracking();
+      setTracking(true);
+    } else {
+      stopAutomaticTracking();
+      setTracking(false);
+    }
+  }
+  //check if location is enable or not
+  const checkIfLocationEnabled= async ()=>{
+    let enabled = await Location.hasServicesEnabledAsync();       //returns true or false
+    if(!enabled){                     //if not enable
+      Alert.alert('Location not enabled', 'Please enable your Location', [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ]);
+    }else{
+      setLocationServicesEnabled(enabled)         //store true into state
+    }
+  }
+  //get current location
+  const getCurrentLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    console.log(status);
+
+    if (status !== 'granted') {
+      Alert.alert('Permission denied', 'Allow the app to use location services', [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ]);
+      return; // Exit the function if permission is not granted
+    }
+
+    try {
+      const { coords } = await Location.getCurrentPositionAsync();
+      console.log(coords);
+
+      if (coords) {
+        setLatitude(coords.latitude);
+        setLongitude(coords.longitude)
+        console.log(coords)
+      }
+    } catch (error) {
+      console.log('Error getting location:', error);
+    }
+  };
+
+  return <View style={styles.container}>
+    <Layout style={styles.layout} level="1">
+      <MapboxGL.MapView style={styles.map}>
+        <MapboxGL.Camera
+            zoomLevel={13}
+            centerCoordinate={[longitude, latitude]}
+            animationMode="flyTo"
+            animationDuration={2000}
+        />
+      </MapboxGL.MapView>
+    </Layout>
+
+    <Button style={styles.button} onPress={changeButton}>
+      {tracking?"stop":"start"}
+    </Button>
+  </View>;
 }
+
+
+
+
+// Define the styles here
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    flexDirection: 'row',
+    margin:5,
+    marginBottom:45,
+    borderRadius:20,
+    height: 600,
+  },
+  container: {
+
+    flex: 1,
+    flexDirection: 'column',
+
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    position: "absolute",
+    bottom: 30, // Position from the bottom of the screen
+    alignSelf: "center", // Center the button horizontally
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 90
+  },
+});
+

d9907151b235897067c937738f0fce42db881e06
MarcoCoreDuo <90222533+MarcoCoreDuo@users.noreply.github.com>
2024-11-12 08:26:55 +0100

    Icon und Splashscreen für light/dark mode (#21)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { Stack, SplashScreen } from "expo-router";
 import * as eva from "@eva-design/eva";
 import { foofDarkTheme } from "@/constants/custom-theme";
 import { ApplicationProvider } from "@ui-kitten/components";
@@ -7,13 +7,46 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { SafeAreaView } from "react-native";
+import { useEffect, useState } from "react";
 
 library.add(far, fas, fab);
 
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [LayoutLoaded, setLayoutLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (LayoutLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [LayoutLoaded]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <ApplicationProvider {...eva} theme={{ ...eva.dark, ...foofDarkTheme }}>
       <SafeAreaView
+        onLayout={() => setLayoutLoaded(true)}
         style={{
           flex: 1,
         }}

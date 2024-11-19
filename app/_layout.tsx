52b0ba9dbd2e5bb53abe586148349463d3eceaed
CeciliaHalbritter <cecilia.halbritter@deutschebahn.com>
2024-11-19 17:12:28 +0100

    Ups, hab light mode angeschaltet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.light }}>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...foofDarkTheme }}>
       <SafeAreaView
         onLayout={() => setLayoutLoaded(true)}
         style={{

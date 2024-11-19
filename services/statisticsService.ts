33af1c88e05c6b72c591d6a5f2976321f3d2c026
CeciliaHalbritter <cecilia.halbritter@deutschebahn.com>
2024-11-19 17:15:50 +0100

    Renamed everything in statisticsService.ts

--- services/statisticsService.ts
+++ services/statisticsService.ts
@@ -1,31 +1,37 @@
-import { Trip } from "@/model/model";
+import { Stage } from "@/model/model";
 import { getDurationFormatted, getDurationInMs, TIME } from "@/utils/dateUtil";
 
 // Distance //
-export function getTripDistanceString(trip: Trip, precision?: number): string {
-  return trip.distance.toFixed(precision ?? 1) + " km";
+export function getStageDistanceString(
+  stage: Stage,
+  precision?: number,
+): string {
+  return stage.distance.toFixed(precision ?? 1) + " km";
 }
 
 // Duration //
-export function getTripDuration(trip: Trip): number {
-  const start = new Date(trip.startedAt);
-  const end = new Date(trip.finishedAt ?? Date.now());
+export function getStageDuration(stage: Stage): number {
+  const start = new Date(stage.startedAt);
+  const end = new Date(stage.finishedAt ?? Date.now());
 
   return getDurationInMs(start, end);
 }
-export function getTripDurationString(trip: Trip): string {
-  const start = new Date(trip.startedAt);
-  const end = new Date(trip.finishedAt ?? Date.now());
+export function getStageDurationString(stage: Stage): string {
+  const start = new Date(stage.startedAt);
+  const end = new Date(stage.finishedAt ?? Date.now());
 
   return getDurationFormatted(start, end, TIME);
 }
 
 // Average Speed //
-export function getTripAvgSpeedInKmh(trip: Trip): number {
-  const hours = getTripDuration(trip) / (1000 * 60 * 60);
+export function getStageAvgSpeedInKmh(stage: Stage): number {
+  const hours = getStageDuration(stage) / (1000 * 60 * 60);
 
-  return trip.distance / hours;
+  return stage.distance / hours;
 }
-export function getTripAvgSpeedString(trip: Trip, precision?: number): string {
-  return getTripAvgSpeedInKmh(trip).toFixed(precision ?? 1) + " km/h";
+export function getStageAvgSpeedString(
+  stage: Stage,
+  precision?: number,
+): string {
+  return getStageAvgSpeedInKmh(stage).toFixed(precision ?? 1) + " km/h";
 }

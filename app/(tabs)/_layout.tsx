3ae3835a92749417f595f6f4497e1d01666369af
MarcoCoreDuo <90222533+MarcoCoreDuo@users.noreply.github.com>
2024-11-25 00:05:37 +0100

    remove unnecessary code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from "expo-router";
 import { foofDarkTheme } from "@/constants/custom-theme";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   BottomNavigation,
   BottomNavigationTab,
@@ -46,44 +45,18 @@ export default function TabLayout() {
         headerStyle: {
           backgroundColor: foofDarkTheme["color-basic-500"],
         },
-        tabBarStyle: {
-          backgroundColor: foofDarkTheme["color-basic-500"],
-        },
-        tabBarActiveTintColor: foofDarkTheme["color-primary-500"],
       }}
     >
       <Tabs.Screen
         name="(home)"
         options={{
-          title: "Home",
           headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <FontAwesomeIcon
-              icon="home"
-              color={
-                focused
-                  ? foofDarkTheme["color-primary-500"]
-                  : foofDarkTheme["color-basic-100"]
-              }
-            />
-          ),
         }}
       />
       <Tabs.Screen
         name="(touren)"
         options={{
-          title: "Touren",
           headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <FontAwesomeIcon
-              icon="map"
-              color={
-                focused
-                  ? foofDarkTheme["color-primary-500"]
-                  : foofDarkTheme["color-basic-100"]
-              }
-            />
-          ),
         }}
       />
     </Tabs>

da51f6dd43d42738ccdc1025087ed31127d42537
Simon Johannes Engelhardt <simon-johannes.engelhardt@deutschebahn.com>
2024-12-03 22:15:19 +0100

    Working tests

--- components/Tour/__tests__/CreateManualStageModal-test.tsx
+++ components/Tour/__tests__/CreateManualStageModal-test.tsx
@@ -1,10 +1,15 @@
 import { render } from "@/test-utils/test-utils";
 import { CreateManualStageModal } from "../CreateManualStageModal";
 
+// Ich vermute das dieser Test nicht mehr nötig ist, da die Komponente nicht mehr existiert.
 describe("<CreateManualStageModal />", () => {
   it("should render correctly", () => {
     const view = render(
-      <CreateManualStageModal isVisible onClose={jest.fn()} tourId="1" />,
+      <CreateManualStageModal
+        isVisible={true}
+        onClose={jest.fn()}
+        tourId="1"
+      />,
     );
     expect(view).toMatchSnapshot();
   });

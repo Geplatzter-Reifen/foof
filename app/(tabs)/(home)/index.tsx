3e81738ded193f31126140810c613378fc307bf4
Cecilia Halbritter <76789481+Fieeesch@users.noreply.github.com>
2024-12-08 16:50:59 +0100

    [GPLR-138] Standort auf Karte (#55)
    Features:
    - Ladespinner während dem Laden der Karte und dem Rest
    - Karte folgt der Userlocation
    - Wenn man die Karte verschiebt, erscheint ein Button zum neu zentrieren

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -6,25 +6,27 @@ import {
   ImageProps,
   Platform,
   StatusBar,
+  TouchableOpacity,
 } from "react-native";
 import * as Location from "expo-location";
 import * as TaskManager from "expo-task-manager";
 import * as Notifications from "expo-notifications";
 
 import {
+  LOCATION_TASK_NAME,
   startAutomaticTracking,
   stopAutomaticTracking,
-  LOCATION_TASK_NAME,
 } from "@/services/tracking";
-import MapboxGL, { Camera } from "@rnmapbox/maps";
+
+import MapboxGL, { Camera, UserTrackingMode } from "@rnmapbox/maps";
+
 import {
   Layout,
   ButtonGroup,
-  IconElement,
+  Spinner,
   Icon,
   TopNavigation,
   Divider,
-  TopNavigationAction,
   Text,
 } from "@ui-kitten/components";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -32,6 +34,7 @@ import BigRoundButton from "@/components/Buttons/BigRoundButton";
 import { getActiveTour } from "@/services/data/tourService";
 import { withObservables } from "@nozbe/watermelondb/react";
 import { Route, Tour } from "@/model/model";
+import { timeout } from "@/utils/utils";
 
 void MapboxGL.setAccessToken(
   "pk.eyJ1Ijoia2F0emFibGFuY2thIiwiYSI6ImNtM2N4am40cTIyZnkydnNjODBldXR1Y20ifQ.q0I522XSqixPNIe6HwJdOg",
@@ -44,37 +47,34 @@ enum ButtonStates {
 }
 
 export default function HomeScreen() {
-  const [latitude, setLatitude] = useState(50.0826); // Default to Wiesbaden
-  const [longitude, setLongitude] = useState(8.24); // Default to Wiesbaden
+  const [loading, setLoading] = useState(true); // Ladezustand
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
   const [buttonState, setButtonState] = useState(ButtonStates.NotCycling);
   const [activeTour, setActiveTour] = useState<Tour>();
+  const [userCentered, setUserCentered] = useState(true);
   const buttonIconSize = 60;
   const camera = useRef<Camera>(null);
 
   let geoJSON: GeoJSON.FeatureCollection | undefined = undefined;
 
   useEffect(() => {
-    void getCurrentLocation();
-    TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME).then((result) => {
-      if (result) {
-        setButtonState(ButtonStates.Cycling);
-      }
-    });
-    getActiveTour().then((tour) => {
-      if (tour) {
-        setActiveTour(tour);
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    camera.current?.setCamera({
-      centerCoordinate: [longitude, latitude],
-      zoomLevel: 13,
-      animationDuration: 2000,
-      animationMode: "flyTo",
-    });
-  }, [latitude, longitude]);
+    const prepare = async () => {
+      await getCurrentLocation();
+      TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME).then((result) => {
+        if (result) {
+          setButtonState(ButtonStates.Cycling);
+        }
+      });
+      getActiveTour().then((tour) => {
+        if (tour) {
+          setActiveTour(tour);
+        }
+      });
+      setLoading(false);
+    };
+    prepare();
+  }, [activeTour]);
 
   const requestPermissionsAsync = async () => {
     return await Notifications.requestPermissionsAsync({
@@ -166,61 +166,78 @@ export default function HomeScreen() {
 
   // Function to display the route on the map by adjusting the camera to fit the route's bounds
   const showRoute = async () => {
-    if (geoJSON) {
-      // Find the outermost coordinates
-      let minLat = Infinity,
-        maxLat = -Infinity,
-        minLng = Infinity,
-        maxLng = -Infinity;
-      geoJSON.features.forEach((feature) => {
-        if (feature.geometry.type === "LineString") {
-          (feature.geometry as GeoJSON.LineString).coordinates.forEach(
-            ([lng, lat]) => {
-              if (lat < minLat) minLat = lat;
-              if (lat > maxLat) maxLat = lat;
-              if (lng < minLng) minLng = lng;
-              if (lng > maxLng) maxLng = lng;
-            },
-          );
-        } else if (feature.geometry.type === "Point") {
-          const [lng, lat] = (feature.geometry as GeoJSON.Point).coordinates;
-          if (lat < minLat) minLat = lat;
-          if (lat > maxLat) maxLat = lat;
-          if (lng < minLng) minLng = lng;
-          if (lng > maxLng) maxLng = lng;
-        }
-      });
-
-      const bounds = {
-        ne: [maxLng, maxLat],
-        sw: [minLng, minLat],
-      };
-
-      camera.current?.setCamera({
-        bounds: bounds,
-        padding: {
-          paddingLeft: 30,
-          paddingRight: 30,
-          paddingTop: 30,
-          paddingBottom: 150,
-        },
-        animationDuration: 2000,
-        heading: 0,
-        animationMode: "flyTo",
-      });
+    if (!geoJSON) {
+      throw new Error("Keine Route importiert!");
     }
+
+    setUserCentered(false);
+    // Find the outermost coordinates
+    let minLat = Infinity,
+      maxLat = -Infinity,
+      minLng = Infinity,
+      maxLng = -Infinity;
+    geoJSON.features.forEach((feature) => {
+      if (feature.geometry.type === "LineString") {
+        (feature.geometry as GeoJSON.LineString).coordinates.forEach(
+          ([lng, lat]) => {
+            if (lat < minLat) minLat = lat;
+            if (lat > maxLat) maxLat = lat;
+            if (lng < minLng) minLng = lng;
+            if (lng > maxLng) maxLng = lng;
+          },
+        );
+      } else if (feature.geometry.type === "Point") {
+        const [lng, lat] = (feature.geometry as GeoJSON.Point).coordinates;
+        if (lat < minLat) minLat = lat;
+        if (lat > maxLat) maxLat = lat;
+        if (lng < minLng) minLng = lng;
+        if (lng > maxLng) maxLng = lng;
+      }
+    });
+
+    const bounds = {
+      ne: [maxLng, maxLat],
+      sw: [minLng, minLat],
+    };
+
+    camera.current?.setCamera({
+      bounds: bounds,
+      padding: {
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingTop: 30,
+        paddingBottom: 150,
+      },
+      animationDuration: 2000,
+      heading: 0,
+      animationMode: "flyTo",
+    });
   };
 
-  const RouteIcon = (props?: Partial<ImageProps>): IconElement => (
-    <Icon
-      {...props}
-      name="route"
-      style={[props?.style, { height: 40, width: "auto" }]}
-    />
+  const CenterButton = (props?: Partial<ImageProps>) => (
+    <TouchableOpacity
+      style={styles.centerButton}
+      onPress={() => setUserCentered(true)}
+    >
+      <Icon
+        {...props}
+        name="location-crosshairs"
+        style={[props?.style, { height: 23 }]}
+      />
+    </TouchableOpacity>
   );
 
-  const renderRouteAction = (): React.ReactElement => (
-    <TopNavigationAction icon={RouteIcon} onPress={showRoute} hitSlop={15} />
+  const RouteButton = (props?: Partial<ImageProps>) => (
+    <TouchableOpacity
+      style={styles.routeButton}
+      onPress={async () => {
+        setUserCentered(false);
+        await timeout(100);
+        showRoute();
+      }}
+    >
+      <Icon {...props} name="route" style={[props?.style, { height: 22 }]} />
+    </TouchableOpacity>
   );
 
   const toggleButtons = (buttonState: ButtonStates) => {
@@ -284,38 +301,78 @@ export default function HomeScreen() {
 
   const EnhancedShapeSourceV2 = enhanceV2(Bridge);
 
+  if (loading) {
+    return (
+      <Layout level="2" style={styles.loadingContainer}>
+        <Spinner size="giant" />
+      </Layout>
+    );
+  }
+
   return (
     <Layout style={styles.container}>
       <Layout>
         <TopNavigation
           title={() => <Text category="h4">Home</Text>}
-          accessoryRight={renderRouteAction}
           style={styles.header}
           alignment="center"
         ></TopNavigation>
         <Divider />
       </Layout>
       <Layout style={styles.layout}>
-        <MapboxGL.MapView style={styles.map}>
+        <MapboxGL.MapView
+          style={styles.map}
+          scaleBarEnabled={false}
+          localizeLabels={{ locale: "current" }}
+          compassEnabled={true}
+          compassPosition={{ top: 8, right: 8 }}
+          logoPosition={{ top: 8, left: 8 }}
+          attributionPosition={{ top: 8, left: 96 }}
+          onTouchMove={() => {
+            setUserCentered(false);
+          }}
+        >
           {activeTour && <EnhancedShapeSourceV2 tour={activeTour} />}
-          <MapboxGL.Camera ref={camera} />
+          <MapboxGL.Camera
+            defaultSettings={{
+              centerCoordinate: [longitude, latitude],
+              zoomLevel: 14,
+            }}
+            followZoomLevel={17}
+            animationMode="flyTo"
+            followUserMode={UserTrackingMode.Follow}
+            followUserLocation={userCentered}
+            ref={camera}
+          />
+          <MapboxGL.UserLocation androidRenderMode="gps" />
         </MapboxGL.MapView>
       </Layout>
+      <View style={styles.mapButtonsContainer}>
+        <RouteButton />
+        {!userCentered && <CenterButton />}
+      </View>
       <View style={styles.button_container}>{toggleButtons(buttonState)}</View>
     </Layout>
   );
 }
 
 const styles = StyleSheet.create({
-  map: {
+  loadingContainer: {
     flex: 1,
-    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     flex: 1,
     flexDirection: "column",
+    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
+    flexDirection: "row",
   },
   header: {
+    flexDirection: "column",
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   layout: {
@@ -328,5 +385,29 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignSelf: "center",
     bottom: 15,
+  },
+  mapButtonsContainer: {
+    position: "absolute",
+    flexDirection: "column",
+    alignSelf: "center",
+    top: 175,
+    right: 11,
+  },
+  centerButton: {
+    backgroundColor: "#fff",
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    elevation: 3,
+    padding: 10.5,
+  },
+  routeButton: {
+    backgroundColor: "#fff",
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    elevation: 3,
+    padding: 10.5,
+    marginBottom: 10,
   },
 });

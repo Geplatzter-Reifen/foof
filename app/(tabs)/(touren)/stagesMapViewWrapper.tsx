82145c9007e132f96d385e27ca8ae31bab557148
annkhalus <anna.khalus@student.hs-rm.de>
2024-12-17 11:32:49 +0100

    stagesMapViewWrapper nothing to refactor, comments added

--- app/(tabs)/(touren)/stagesMapViewWrapper.tsx
+++ app/(tabs)/(touren)/stagesMapViewWrapper.tsx
@@ -3,6 +3,20 @@ import { useLocalSearchParams } from "expo-router";
 import { Layout, Text } from "@ui-kitten/components";
 import StagesMapView from "../../../components/Tour/StagesMapView";
 
+/**
+ * StagesMapViewWrapper Component
+ *
+ * This wrapper component extracts the `tourId` parameter from the URL using `useLocalSearchParams`.
+ * - If `tourId` is not provided, it displays an error message.
+ * - If `tourId` is available, it passes it as a prop to the `StagesMapView` component.
+ *
+ * @returns {JSX.Element}
+ * - An error message if `tourId` is missing.
+ * - The `StagesMapView` component with the `tourId` prop if it's available.
+ *
+ * Usage:
+ * This component acts as a bridge to inject the `tourId` parameter into the `StagesMapView` component.
+ */
 type localParams = {
   tourId?: string;
 };

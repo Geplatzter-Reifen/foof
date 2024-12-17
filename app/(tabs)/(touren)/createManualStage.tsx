6429d6bc160f7a83a3a7bc3258b464c0d9bdce16
annkhalus <anna.khalus@student.hs-rm.de>
2024-12-17 10:52:15 +0100

    createManualStage, CardComponent refactoring, comments done

--- app/(tabs)/(touren)/createManualStage.tsx
+++ app/(tabs)/(touren)/createManualStage.tsx
@@ -1,36 +1,69 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { Text, Layout, Button } from "@ui-kitten/components";
-import ButtonGroup from "../../../components/Buttons/ButtonGroup";
-import CoordinateInput from "../../../components/Stage/CoordinateInput";
-import { StyleSheet } from "react-native";
-import CardComponent from "../../../components/Stage/CardComponent";
+import { Alert, StyleSheet } from "react-native";
+import { Button, Layout, Text } from "@ui-kitten/components";
+import { Input } from "@ui-kitten/components";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useLocalSearchParams, useNavigation, useRouter } from "expo-router";
-import { Input } from "@ui-kitten/components";
+import ButtonGroup from "../../../components/Buttons/ButtonGroup";
+import CoordinateInput from "../../../components/Stage/CoordinateInput";
+import CardComponent from "../../../components/Stage/CardComponent";
 import { createManualStage as createManualStageFn } from "@/services/tracking";
-import { Alert } from "react-native";
 import { ButtonSwitch } from "@/components/Buttons/ButtonSwitch";
 
+/**
+ * useCoordinates function
+ *
+ * This function is a custom hook for managing latitude, longitude, and date states.
+ *
+ * @param {Object} params - The initial parameters for the hook.
+ * @param {string} [params.initialLatitude] - The initial latitude value.
+ * @param {string} [params.initialLongitude] - The initial longitude value.
+ * @param {Date} [params.initialDate] - The initial date value.
+ *
+ * @returns {Object} An object containing the latitude, longitude, and date states, along with their setters.
+ */
+type useCoordinateParams = {
+  initialLatitude?: string;
+  initialLongitude?: string;
+  initialDate?: Date;
+};
+
+function useCoordinates({
+  initialLatitude = "",
+  initialLongitude = "",
+  initialDate = new Date(),
+}: useCoordinateParams = {}) {
+  const [latitude, setLatitude] = useState(initialLatitude);
+  const [longitude, setLongitude] = useState(initialLongitude);
+  const [date, setDate] = useState(initialDate);
+
+  return { latitude, setLatitude, longitude, setLongitude, date, setDate };
+}
+
+/**
+ * CreateManualStage Component
+ *
+ * Renders a form to manually create a stage in a tour. Includes:
+ * - Start and end coordinates.
+ * - Start and end dates.
+ * - Title editing functionality.
+ * - Submission and cancellation options.
+ *
+ * @returns {JSX.Element} The rendered component.
+ */
 const CreateManualStage: React.FC = () => {
   const { tourId } = useLocalSearchParams<{ tourId: string }>();
-  // changing the title of the page
+  if (!tourId) throw new Error("Missing tourId parameter.");
+
   const navigation = useNavigation();
-  //switches title from plain text to the input field
-  const [titleBeingChanged, setTitleBeingChanged] = useState(false);
-  const [stageTitle, setStageTitle] = useState("Etappe"); ///the name of the title
   const router = useRouter();
-  // compass input
-  const [startLatitude, setStartLatitude] = useState("");
-  const [startLongitude, setStartLongitude] = useState("");
-  const [startDate, setStartDate] = useState(new Date());
 
-  const [endLatitude, setEndLatitude] = useState("");
-  const [endLongitude, setEndLongitude] = useState("");
-  const [endDate, setEndDate] = useState(new Date());
+  const [titleBeingChanged, setTitleBeingChanged] = useState(false);
+  const [stageTitle, setStageTitle] = useState("Etappe");
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  ////////////address input
-
-  ////////////on the map input
+  const start = useCoordinates();
+  const end = useCoordinates();
 
   const titleInput = useMemo(
     () => (
@@ -39,19 +72,20 @@ const CreateManualStage: React.FC = () => {
           value={stageTitle}
           onChangeText={(nextValue) => setStageTitle(nextValue)}
           maxLength={20}
+          accessibilityLabel="Stage Title Input"
+          placeholder="Enter stage title"
         />
       </Layout>
     ),
     [stageTitle],
   );
 
-  //switching between done and edit
-
   const changingTitleButton = useMemo(
     () => (
       <Button
         appearance="ghost"
         onPress={() => setTitleBeingChanged((prevState) => !prevState)}
+        accessibilityLabel="Toggle title edit mode"
       >
         {titleBeingChanged ? (
           <FontAwesomeIcon icon="check" size={25} />
@@ -63,17 +97,15 @@ const CreateManualStage: React.FC = () => {
     [titleBeingChanged],
   );
 
-  // Update header input ot text based on if title or edit state changes
   useEffect(() => {
     navigation.setOptions({
-      headerTitle: () =>
-        titleBeingChanged ? (
-          titleInput
-        ) : (
-          <Text category="h4">{stageTitle}</Text>
-        ), // Switch between input and title
-      headerRight: () => changingTitleButton, // Set the button on the right
-      headerTitleAlign: "center", // Keep the title centered
+      headerTitle: titleBeingChanged ? (
+        titleInput
+      ) : (
+        <Text category="h4">{stageTitle}</Text>
+      ),
+      headerRight: () => changingTitleButton,
+      headerTitleAlign: "center",
     });
   }, [
     navigation,
@@ -83,49 +115,70 @@ const CreateManualStage: React.FC = () => {
     stageTitle,
   ]);
 
-  const startCoordInput = (
+  const renderCoordinateInput = (
+    latitude: string,
+    longitude: string,
+    setLatitude: React.Dispatch<React.SetStateAction<string>>,
+    setLongitude: React.Dispatch<React.SetStateAction<string>>,
+    date: Date,
+    setDate: React.Dispatch<React.SetStateAction<Date>>,
+  ) => (
     <CoordinateInput
-      latitude={startLatitude}
-      longitude={startLongitude}
-      setLatitude={setStartLatitude}
-      setLongitude={setStartLongitude}
-      date={startDate}
-      setDate={setStartDate}
+      latitude={latitude}
+      longitude={longitude}
+      setLatitude={setLatitude}
+      setLongitude={setLongitude}
+      date={date}
+      setDate={setDate}
     />
   );
 
-  const endCoordInput = (
-    <CoordinateInput
-      latitude={endLatitude}
-      longitude={endLongitude}
-      setLatitude={setEndLatitude}
-      setLongitude={setEndLongitude}
-      date={endDate}
-      setDate={setEndDate}
-    />
+  const startCoordInput = renderCoordinateInput(
+    start.latitude,
+    start.longitude,
+    start.setLatitude,
+    start.setLongitude,
+    start.date,
+    start.setDate,
+  );
+
+  const endCoordInput = renderCoordinateInput(
+    end.latitude,
+    end.longitude,
+    end.setLatitude,
+    end.setLongitude,
+    end.date,
+    end.setDate,
   );
 
   const onSubmitStage = async () => {
+    if (!stageTitle.trim()) {
+      Alert.alert("Error", "Stage title cannot be empty.");
+      return;
+    }
+
+    if (!start.latitude || !end.latitude) {
+      Alert.alert("Error", "Coordinates cannot be empty.");
+      return;
+    }
+
     try {
       await createManualStageFn(
         stageTitle,
-        startLatitude + ", " + startLongitude,
-        endLatitude + ", " + endLongitude,
-        startDate,
-        endDate,
+        `${start.latitude}, ${start.longitude}`,
+        `${end.latitude}, ${end.longitude}`,
+        start.date,
+        end.date,
         tourId,
       );
       router.back();
     } catch (err) {
-      if (err instanceof Error) {
-        Alert.alert("Error", err.message);
-      } else {
-        Alert.alert("Unknown Error", "An unexpected error occurred.");
-      }
+      Alert.alert(
+        "Error",
+        err instanceof Error ? err.message : "Unknown Error",
+      );
     }
   };
-
-  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <Layout style={styles.layout} level="2">
@@ -133,32 +186,35 @@ const CreateManualStage: React.FC = () => {
         onSelect={(index) => setSelectedIndex(index)}
         selectedIndex={selectedIndex}
       >
-        <Button style={styles.button}>
+        <Button style={styles.button} accessibilityLabel="Compass button">
           <FontAwesomeIcon icon="compass" size={25} />
         </Button>
-        <Button style={styles.button}>
+        <Button style={styles.button} accessibilityLabel="Map Pin button">
           <FontAwesomeIcon icon="map-pin" size={25} />
         </Button>
-        <Button style={styles.button}>
+        <Button style={styles.button} accessibilityLabel="City button">
           <FontAwesomeIcon icon="city" size={25} />
         </Button>
       </ButtonSwitch>
 
       <Layout style={styles.cardsContainer} level="2">
         <CardComponent title="Start" form={startCoordInput} />
-
         <CardComponent title="Ende" form={endCoordInput} />
       </Layout>
+
       <ButtonGroup>
         <Button
           style={styles.button}
-          onPress={() => {
-            router.back();
-          }}
+          onPress={() => router.back()}
+          accessibilityLabel="Cancel button"
         >
           <Text category="h1">ABBRECHEN</Text>
         </Button>
-        <Button style={styles.button} onPress={onSubmitStage}>
+        <Button
+          style={styles.button}
+          onPress={onSubmitStage}
+          accessibilityLabel="Submit button"
+        >
           <Text category="h1">ERSTELLEN</Text>
         </Button>
       </ButtonGroup>
@@ -170,8 +226,8 @@ export default CreateManualStage;
 
 const styles = StyleSheet.create({
   cardsContainer: {
-    flex: 7, // Takes up the majority of the remaining space
-    flexDirection: "column", // Arrange cards in a column
+    flex: 7,
+    flexDirection: "column",
   },
   layout: {
     flex: 1,
--- components/Stage/CardComponent.tsx
+++ components/Stage/CardComponent.tsx
@@ -1,36 +1,45 @@
 import React from "react";
-
 import { Card, Text, Layout, useTheme } from "@ui-kitten/components";
 import { StyleSheet } from "react-native";
 
+/**
+ * CardComponent
+ *
+ * A reusable card component with a customizable title and content.
+ * This component is styled using Eva Design System's `Card` and provides a
+ * visually appealing card layout with a header.
+ *
+ * @param {Object} props - The properties for the card.
+ * @param {string} props.title - The title displayed at the top of the card.
+ * @param {React.ReactNode} props.form - The content rendered inside the card under the header.
+ * @returns {JSX.Element} A card component with a header and customizable content.
+ */
 interface cardProps {
-  form: React.ReactElement;
+  form: React.ReactNode;
   title: string;
 }
 
-const CardHeder = ({ title }: { title: string }): React.ReactElement => {
+const CardHeader = React.memo(({ title }: { title: string }) => {
   const theme = useTheme();
   return (
     <Text
-      style={{
-        textAlign: "center",
-        padding: 5,
-        borderBottomColor: theme["color-basic-500"],
-      }}
+      style={StyleSheet.flatten([
+        style.cardHeader,
+        { borderBottomColor: theme["color-basic-500"] },
+      ])}
       category="h4"
     >
       {title}
     </Text>
   );
-};
+});
 
-function CardComponent(props: cardProps) {
-  const { form, title } = props;
+const CardComponent: React.FC<cardProps> = ({ form, title }) => {
   return (
     <Layout style={style.flexContainer} level="2">
       <Card
         style={style.card}
-        header={() => CardHeder({ title })}
+        header={() => <CardHeader title={title} />}
         pointerEvents="none"
         disabled={true}
         pressableProps={{
@@ -41,7 +50,7 @@ function CardComponent(props: cardProps) {
       </Card>
     </Layout>
   );
-}
+};
 
 export default CardComponent;
 
@@ -57,6 +66,11 @@ const style = StyleSheet.create({
     shadowRadius: 2,
     // Schatten für Android
     elevation: 3,
+  },
+  cardHeader: {
+    textAlign: "center",
+    padding: 5,
+    borderBottomWidth: 1,
   },
   flexContainer: {
     flex: 1,

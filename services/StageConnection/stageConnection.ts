b7d9e03d38963d4fef8200700b8f6bf3fb92576e
Simon Johannes Engelhardt <simon-johannes.engelhardt@deutschebahn.com>
2024-12-17 08:56:52 +0100

    File with comment of the pseudo code

---
+++ services/StageConnection/stageConnection.ts
@@ -0,0 +1,22 @@
+/*
+linked_list = []
+etappen_list_sorted
+for (int i = 0; i < etappen_list_sorted.lenght; i++):
+  erste_location_i
+  letzte_location_i
+  linked_list.push(etappen_list_sorted[i])
+  for(int j = i+1; j < etappen_list_sorted.length; j++):
+    erste_location_j
+    letzte_location_j
+    if (in_area(erste_location_i, erste_location_j))
+      # in linked list einfügen mit 
+      # prev = null und 
+      # next=etappen_list_sorted[j]
+      
+    else if (in_area(erste_location_i, letzte_location_j))
+      ...
+    else if (in_area(letzte_location_i, erste_location_j))
+      ...
+    else if (in_area(letzte_location_i, letzte_location_j))
+      ...
+    */

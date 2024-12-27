ea942a39095bdc3dcb9e7543bda4278d2f276526
CeciliaHalbritter <cecilia.halbritter@deutschebahn.com>
2024-12-27 13:38:45 +0100

    Teilen und Löschen aus der Etappenliste entfernt

--- components/Tour/StageCard.tsx
+++ components/Tour/StageCard.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import { DateFormat, formatDate } from "@/utils/dateUtil";
 
 import { Stage } from "@/database/model/model";
-import { deleteStage } from "@/services/data/stageService";
-import { shareStage } from "@/services/sharingService";
 import {
   getStageAvgSpeedString,
   getStageDistanceString,
   getStageDurationString,
 } from "@/services/statisticsService";
 
-import { Button, Card, Icon, IconElement, Text } from "@ui-kitten/components";
-import { ImageProps, StyleSheet, View } from "react-native";
+import { Card, Text } from "@ui-kitten/components";
+import { StyleSheet, View } from "react-native";
 import customStyles from "../../constants/styles";
 import { withObservables } from "@nozbe/watermelondb/react";
 import { router } from "expo-router";
@@ -24,44 +22,12 @@ function StageCardComponent({ stage }: { stage: Stage }) {
   const distanceString: string = getStageDistanceString(stage);
   const avgSpeedString: string = getStageAvgSpeedString(stage);
 
-  // Icons für Teilen und Löschen
-  const ShareIcon = (props?: Partial<ImageProps>): IconElement => (
-    <Icon
-      {...props}
-      name="share-nodes"
-      style={[props?.style, { height: 18, width: "auto" }]}
-    />
-  );
-  const TrashIcon = (props?: Partial<ImageProps>): IconElement => (
-    <Icon
-      {...props}
-      name="trash"
-      style={[props?.style, { height: 18, width: "auto" }]}
-    />
-  );
-
-  // Header der Kachel mit Löschen- und Teilen-Button
+  // Header der Kachel
   const Header = () => {
     return (
-      <View style={styles.header}>
-        <Text category="h5" style={styles.title}>
-          {stage.title}
-        </Text>
-        <View style={styles.headerButtonGroup}>
-          <Button
-            status="basic"
-            appearance="ghost"
-            accessoryLeft={TrashIcon}
-            onPress={() => deleteStage(stage.id)}
-          ></Button>
-          <Button
-            status="basic"
-            appearance="ghost"
-            accessoryLeft={ShareIcon}
-            onPress={() => shareStage(stage)}
-          ></Button>
-        </View>
-      </View>
+      <Text category="h5" style={styles.title}>
+        {stage.title}
+      </Text>
     );
   };
 
@@ -129,13 +95,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     flex: 1,
   },
-  header: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
   title: {
-    marginTop: 8,
+    marginVertical: 8,
     marginLeft: 15,
   },
   body: {
@@ -143,9 +104,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-between",
-  },
-  headerButtonGroup: {
-    flexDirection: "row",
   },
   date: {
     marginTop: 9,

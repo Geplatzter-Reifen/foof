379a7d7e7ec076efc528566be123d0bb1559e173
annkhalus <anna.khalus@student.hs-rm.de>
2025-01-06 17:33:27 +0100

    removed settings container added missing comments

--- app/(tabs)/(touren)/settings.tsx
+++ app/(tabs)/(touren)/settings.tsx
@@ -1,97 +1,48 @@
-import React, { useState } from "react";
-import {
-  Icon,
-  IconElement,
-  Input,
-  Text,
-  ThemeType,
-  TopNavigationAction,
-  useTheme,
-} from "@ui-kitten/components";
-import { ImageProps, StyleSheet, Alert } from "react-native";
-import { useLocalSearchParams } from "expo-router";
-import { updateTourNameById } from "@/services/data/tourService";
-import { setTourRoute } from "@/services/data/routeService";
-import * as DocumentPicker from "expo-document-picker";
-import * as FileSystem from "expo-file-system";
-import * as Gjv from "geojson-validation";
-import SettingsContainer from "@/components/SettingsConatiner/SettingsContainer";
-import SingleSettingLayout from "@/components/SingleSettingLayout/SingleSettingLayout";
-import InlineRow from "@/components/InlineRow/InlineRow";
+import React from "react";
+import { Card, Layout, ThemeType, useTheme } from "@ui-kitten/components";
+import { StyleSheet } from "react-native";
+
 import RouteGuidelineSettingsSection from "@/components/RouteGuidelineSettingsSection/RouteGuidelineSettingsSection";
 import TourNameSettingsSection from "@/components/TourNameSettingsSection/TourNameSettingsSection";
-
-const OkayIcon = (props?: Partial<ImageProps>): IconElement => (
-  <Icon {...props} name="check" style={[props?.style, { height: 24 }]} />
-);
-
-const EditIcon = (props?: Partial<ImageProps>): IconElement => (
-  <Icon {...props} name="edit" style={[props?.style, { height: 24 }]} />
-);
-
-type TourenParams = {
-  tourId: string;
-  tourTitle: string;
-};
+import customStyles from "@/constants/styles";
 
 export default function Settings() {
-  const params = useLocalSearchParams() as TourenParams;
-  const { tourId, tourTitle } = params;
-  const [tourname, setTourname] = useState(tourTitle);
-  const [titleBeingEdited, setTitleBeingEdited] = useState(false);
-  const [selectedFile, setSelectedFile] =
-    useState<DocumentPicker.DocumentPickerResult>();
-
   const theme = useTheme();
   const styles = makeStyles(theme);
-
-  const updateTourname = (newTourName: string) => {
-    updateTourNameById(tourId, newTourName).then(() => {
-      setTourname(newTourName);
-      setTitleBeingEdited(false);
-    });
-  };
-
-  const importTour = async () => {
-    const file = await DocumentPicker.getDocumentAsync({
-      type: "application/json",
-    });
-    if (!file.canceled) {
-      const content = await FileSystem.readAsStringAsync(file.assets[0].uri);
-      if (Gjv.valid(JSON.parse(content))) {
-        setSelectedFile(file);
-        await setTourRoute(tourId, content);
-      } else {
-        Alert.alert("Fehler", "Die Datei ist kein gültiges GeoJSON");
-      }
-    }
-  };
   return (
-    <SettingsContainer>
-      <>
-        <TourNameSettingsSection />
-        <RouteGuidelineSettingsSection />
-      </>
-    </SettingsContainer>
+    <Layout style={styles.container} level="2">
+      <Layout style={styles.centeringContainer} level="2">
+        <Card
+          style={{
+            ...customStyles.basicCard,
+            ...customStyles.basicShadow,
+            ...styles.card,
+          }}
+          disabled={true}
+        >
+          <TourNameSettingsSection />
+          <RouteGuidelineSettingsSection />
+        </Card>
+      </Layout>
+    </Layout>
   );
 }
 
 const makeStyles = (theme: ThemeType) =>
   StyleSheet.create({
-    successMessage: {
+    container: {
+      flex: 1,
+    },
+    centeringContainer: {
+      flex: 1,
+    },
+    card: {
       marginTop: 10,
-      color: theme["color-primary-600"],
+      alignSelf: "flex-start",
+      paddingHorizontal: 15,
+      marginHorizontal: 10,
     },
-    input: {
-      marginTop: 5,
-      alignSelf: "stretch",
-      width: "80%",
-    },
-    description: {
-      marginTop: 5,
-      color: theme["color-basic-500"],
-    },
-    accentText: {
-      color: theme["color-primary-600"],
+    settingContainer: {
+      marginBottom: 15,
     },
   });

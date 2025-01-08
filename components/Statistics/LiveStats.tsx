eb3b646eecc00ef3d11890bc3a4e09a54d008235
Tobias Hermann <tobias.hermann@student.hs-rm.de>
2025-01-08 16:17:35 +0100

    Average speed no, current speed yes

--- components/Statistics/LiveStats.tsx
+++ components/Statistics/LiveStats.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet, View } from "react-native";
 import { Card } from "@ui-kitten/components";
 import {
-  getStageAvgSpeedString,
   getStageDistanceString,
   getStageDurationString,
 } from "@/services/statisticsService";
@@ -9,9 +8,40 @@ import { Stage } from "@/database/model/model";
 import { withObservables } from "@nozbe/watermelondb/react";
 import customStyles from "@/constants/styles";
 import IconStat from "@/components/Statistics/IconStat";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { LocationSubscription } from "expo-location";
+import { msToKmh } from "@/utils/unitUtils";
 
 function MapStatisticsBox({ stage }: { stage: Stage }) {
+  const [speed, setSpeed] = useState("0"); // Speed in meters/second
+
+  useEffect(() => {
+    let subscription: LocationSubscription;
+
+    const startLocationSubscription = async () => {
+      subscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 1000,
+          distanceInterval: 1,
+        },
+        (location) => {
+          const speed = location.coords.speed || 0;
+          setSpeed(msToKmh(speed).toFixed(2));
+        },
+      );
+    };
+
+    startLocationSubscription();
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, []);
+
   if (!stage) {
     return null;
   }
@@ -55,7 +85,7 @@ function MapStatisticsBox({ stage }: { stage: Stage }) {
           iconWidth={30}
           iconHeight={30}
         >
-          {getStageAvgSpeedString(stage)}
+          {speed}
         </IconStat>
       </View>
     </Card>
